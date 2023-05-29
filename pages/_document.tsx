73dba447bebec3bdc26e22aaee2f0fcaf116034c
John Roller <jtroller33@gmail.com>
2023-05-28 21:46:49 -0500

    tailwindCSS config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="John Roller Next.js Portfolio" />
+      
+      
+      </Head>
       <body>
         <Main />
         <NextScript />
