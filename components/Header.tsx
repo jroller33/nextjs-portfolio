0085f1a87dce775d10b932f8f55e362fe009297c
John Roller <jtroller33@gmail.com>
2023-05-28 23:07:25 -0500

    social icons on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { SocialIcon } from "react-social-icons";
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <div>Header</div>
+    <header>
+        <div className='flex flex-row items-center'>
+            {/* Social icons */}
+
+            <SocialIcon 
+                url='https://www.youtube.com/@johnr8729/videos'
+                network='youtube'
+                fgColor='gray'
+                bgColor='transparent'
+            />
+            <SocialIcon 
+                url='www.github.com/jroller33'
+                network='github'
+                fgColor='gray'
+                bgColor='transparent'
+            />
+            <SocialIcon 
+                url='www.youtube.com/sonnysangha'
+                fgColor='gray'
+                bgColor='transparent'
+            />
+        </div>
+
+        <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+            <SocialIcon 
+            className='cursor-pointer'
+            network='email'
+            fgColor='gray'
+            bgColor='transparent'
+            />
+            <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Contact Me</p>
+        </div>
+    </header>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,8 @@ export default function Home() {
       </Head>
 
       {/* Header */}
+
+      <Header />
 
       {/* Hero */}
 
