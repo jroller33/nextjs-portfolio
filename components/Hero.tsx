c52ba658e4f2a730ad19114e4177c8ebfb9ed465
John Roller <jtroller33@gmail.com>
2023-05-29 21:20:01 -0500

    typewriter animation is working

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Cursor, useTypewriter } from "react-simple-typewriter"
 
 type Props = {}
 
 export default function Hero({}: Props) {
+    const [text, count] = useTypewriter({
+        words: [
+        "Hi, my name's John Roller",
+        "I use a lot of JavaScript,",
+        "But I like Python more.",
+        "Scroll down to see my projects!",
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    });
+
+
     return (
-    <div>Hero</div>
+    <div>
+        <p>{text}</p>
+    </div>
     )
 }
