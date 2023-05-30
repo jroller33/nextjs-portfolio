be2d77958e2d35604474289578bb150a2e25cebc
John Roller <jtroller33@gmail.com>
2023-05-29 20:53:28 -0500

    starting on Hero component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,9 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import Header from '@/components/Header'
+import Hero from '@/components/Hero'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] })     // I'm not sure what this does, but I don't want to delete it yet
 
 export default function Home() {
   return (
@@ -12,9 +13,14 @@ export default function Home() {
         <title>John Roller Portfolio</title> 
       </Head>
 
+
       <Header />
 
+
       {/* Hero */}
+      <section id="hero">
+        <Hero />
+      </section>
 
       {/* About */}
 
