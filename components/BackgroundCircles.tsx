dd85bc46af0f7d1ef75c21e58bfca0e2d2e6fd75
John Roller <jtroller33@gmail.com>
2023-05-29 22:02:41 -0500

    starting on BackgroundCircles

---
+++ components/BackgroundCircles.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+type Props = {}
+
+function BackgroundCircles({}: Props) {
+    return (
+    <div>
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+    </div>
+    )
+}
+
+export default BackgroundCircles
