c37763a06cd43739542a83feb4fae699e4c8e1e4
John Roller <jtroller33@gmail.com>
2023-05-31 17:13:35 -0500

    Image component for Hero is working, but not sized correctly

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Cursor, useTypewriter } from "react-simple-typewriter"
 import BackgroundCircles from '@/components/BackgroundCircles'
+import Image from 'next/image'
+// import HeroImage from '../public/HeroImage.png'
 
 type Props = {}
 
@@ -19,8 +21,13 @@ export default function Hero({}: Props) {
 
     return (
     <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+        
         <BackgroundCircles />
-        <img src='' alt='' />
+
+        <div className='relative h-96 w-96'>
+            <Image src='/HeroImage.png' alt='Hero Image' fill={true} />
+        </div>
+
         <h1>
             <span>{text}</span>
             <Cursor cursorColor="#ffea00" />
