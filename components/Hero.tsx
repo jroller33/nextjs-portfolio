291f98e98491a4a305c8cc09d869663de7a80370
John Roller <jtroller33@gmail.com>
2023-05-31 21:23:26 -0500

    added Links to Hero section. about to fix z-index issues on Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Cursor, useTypewriter } from "react-simple-typewriter"
 import BackgroundCircles from '@/components/BackgroundCircles'
 import Image from 'next/image'
+import Link from 'next/link'
 // import HeroImage from '../public/HeroImage.png'
 
 type Props = {}
@@ -24,19 +25,40 @@ export default function Hero({ }: Props) {
 
             <BackgroundCircles />
 
-                {/* Hero Image */}
+            {/* Hero Image */}
             <div className='relative rounded-full h-32 w-32 mx-auto object-contain'>
                 <Image priority src='/HeroImageTransparent.png' alt='Hero Image' fill={true} />
             </div>
 
 
-                {/* Typewriter Text */}
             <div>
-                <h2 className='text-sm uppercase text-gray-400 pb-2 tracking-[12px]'>Full-Stack Developer</h2>
-                <h1>
-                    <span>{text}</span>
+                <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[10px]'>Full-Stack Developer</h2>
+
+                {/* Typewriter Text */}
+                <h1 className='text-4xl lg:text-5xl font-semibold px-10'>
+                    <span className='mr-1'>{text}</span>
                     <Cursor cursorColor="#ffea00" />
                 </h1>
+
+                <div className='pt-5'>
+
+                    <Link href="#about">
+                        <button className='heroButton'>About</button>
+                    </Link>
+
+                    <Link href="#experience">
+                        <button className='heroButton'>Experience</button>
+                    </Link>
+
+                    <Link href="#skills">
+                        <button className='heroButton'>Skills</button>
+                    </Link>
+
+                    <Link href="#projects">
+                        <button className='heroButton'>Projects</button>
+                    </Link>
+                    
+                </div>
             </div>
         </div>
     )
