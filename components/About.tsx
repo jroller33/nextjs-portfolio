37ff62f7c9e871c35b7c6e9b2b16e634b8081eac
John Roller <jtroller33@gmail.com>
2023-06-01 13:06:39 -0500

    About section is DONE

--- components/About.tsx
+++ components/About.tsx
@@ -7,7 +7,11 @@ type Props = {}
 
 export default function About({ }: Props) {
   return (
-    <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    <motion.div
+    initial={{ opacity: 0 }}
+    whileInView={{ opacity: 1 }}
+    transition={{ duration: 1.5 }}
+    className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
 
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         About
@@ -44,6 +48,6 @@ export default function About({ }: Props) {
 
       </div>
 
-    </div>
+    </motion.div>
   )
 }
