c7e8db27355d38445d6880bcaf57e6c29fa59bb7
John Roller <jtroller33@gmail.com>
2023-06-01 11:21:28 -0500

    working on About section image

--- components/About.tsx
+++ components/About.tsx
@@ -1,11 +1,31 @@
 // trfc
 
 import React from 'react'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
 export default function About({}: Props) {
   return (
-    <div>About</div>
+    <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+        About
+      </h3>
+      
+      <motion.img 
+      initial={{ 
+        x: -200,
+        opacity: 0,
+      }}
+      transition={{ duration: 1.2 }}
+      whileInView={{
+        opacity: 1, 
+        x: 0,
+      }}
+      viewport={{ once: true }}
+      src=''
+      />
+
+    </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
  
 
       {/* Hero */}
-      <section id="hero" className='snap-center'>
+      <section id="hero" className='snap-start'>
         <Hero />
       </section>
 
