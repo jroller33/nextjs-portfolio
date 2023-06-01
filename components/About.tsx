1fb541e374a7c3060b361fadbb2b5dfe13480d16
John Roller <jtroller33@gmail.com>
2023-06-01 11:54:08 -0500

    temporary About image is sizing correctly

--- components/About.tsx
+++ components/About.tsx
@@ -8,6 +8,7 @@ type Props = {}
 export default function About({}: Props) {
   return (
     <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+      
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         About
       </h3>
@@ -17,13 +18,17 @@ export default function About({}: Props) {
         x: -200,
         opacity: 0,
       }}
+
       transition={{ duration: 1.2 }}
+
       whileInView={{
         opacity: 1, 
         x: 0,
       }}
+
       viewport={{ once: true }}
-      src=''
+      className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
+      src='/AboutAlt.jpg' alt='About Image'
       />
 
     </div>
