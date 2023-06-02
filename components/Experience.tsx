1f8c28f30ac94b38ad66d46e6b5b2afadb2aabce
John Roller <jtroller33@gmail.com>
2023-06-02 13:23:38 -0500

    Experience section is supposedly finished, but layout is terrible

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -16,7 +16,7 @@ export default function Experience({}: Props) {
             Experience
         </h3>
 
-        <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
+        <div className='w-full flex space-x-5 overflow-x-auto p-10 snap-x snap-mandatory'>
             <ExperienceCard />
             <ExperienceCard />
             <ExperienceCard />
