cc97e450c8382b0258f44db315d8a0bddbe8ccd6
John Roller <jtroller33@gmail.com>
2023-06-03 02:08:33 -0500

    added Prop for Skill animation

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
-type Props = {}
+type Props = {
+    directionLeft?: boolean;
+}
 
-export default function Skill({}: Props) {
-  return (
-    <div>Skill</div>
-  )
+export default function Skill({ directionLeft }: Props) {
+    return (
+        <div className='group relative flex cursor-pointer'>
+            <motion.img                 // This needs to be an Image component wrapped in <motion.div>
+                initial={{
+                    x: directionLeft ? -200 : 200,
+                }}
+            />
+        </div>
+    )
 }
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -4,9 +4,14 @@ import Skill from './Skill'
 
 type Props = {}
 
-export default function Skills({}: Props) {
+export default function Skills({ }: Props) {
   return (
-    <motion.div className='min-h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 justify-center xl:space-y-0 mx-auto items-center'>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className='min-h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 justify-center xl:space-y-0 mx-auto items-center'>
+
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Skills
       </h3>
@@ -14,6 +19,14 @@ export default function Skills({}: Props) {
       <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover over a skill for proficiency</h3>
 
       <div className='grid grid-cols-4 gap-5'>
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
         <Skill />
       </div>
 
