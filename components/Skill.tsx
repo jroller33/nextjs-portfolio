1677e8142a0a3d03aa2aeab02e43374d917d3358
John Roller <jtroller33@gmail.com>
2023-06-03 01:56:11 -0500

    added Skill component for Skills

---
+++ components/Skill.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Skill({}: Props) {
+  return (
+    <div>Skill</div>
+  )
+}
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import Skill from './Skill'
 
 type Props = {}
 
@@ -11,6 +12,12 @@ export default function Skills({}: Props) {
       </h3>
 
       <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover over a skill for proficiency</h3>
+
+      <div className='grid grid-cols-4 gap-5'>
+        <Skill />
+      </div>
+
+
     </motion.div>
   )
 }
