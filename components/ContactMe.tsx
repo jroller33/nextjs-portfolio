1e78be8bf0390876173f085a9c07d422da8a0099
John Roller <jtroller33@gmail.com>
2023-06-08 10:59:50 -0500

    added new animations to Header and ContactMe

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -37,14 +37,14 @@ export default function ContactMe({ }: Props) {
 
         <form className='flex flex-col space-y-2 w-fit mx-auto'>
           <div className='flex space-x-2'>
-            <input className='contactInput' type="text" />
-            <input className='contactInput' type="text" />
+            <input placeholder="Name" className='contactInput' type="text" />
+            <input placeholder="Email" className='contactInput' type="text" />
           </div>
 
-          <input className='contactInput' type='text' />
+          <input placeholder="Subject" className='contactInput' type='text' />
 
-          <textarea className='contactInput'/>
-          <button>Submit</button>
+          <textarea placeholder="Message" className='contactInput'/>
+          <button className='bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg hover:bg-slate-600 hover:text-[#F7AB0A] hover:animate-pulse'>Submit</button>
 
 
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -31,14 +31,14 @@ return (
                 network='youtube'
                 fgColor='currentColor'
                 bgColor='transparent'
-                className='text-gray-500 hover:text-[#ffea00]/60 hover:animate-slow-ping'
+                className='text-gray-500 hover:text-[#ffea00]/60 hover:animate-pulse'
             />
             <SocialIcon 
                 url='https://www.github.com/jroller33'
                 network='github'
                 fgColor='currentColor'
                 bgColor='transparent'
-                className='text-gray-500 hover:text-[#ffea00]/60 hover:animate-slow-ping'
+                className='text-gray-500 hover:text-[#ffea00]/60 hover:animate-pulse'
             />
 
         </motion.div>
@@ -64,7 +64,7 @@ return (
             network='email'
             fgColor='currentColor'
             bgColor='transparent'
-            className='cursor-pointer text-gray-500 hover:text-[#ffea00]/60 hover:animate-slow-ping'
+            className='cursor-pointer text-gray-500 hover:text-[#ffea00]/60 hover:animate-pulse'
             />
             <p className='uppercase hidden md:inline-flex text-sm text-gray-500 '>Contact Me</p>
         </motion.div>
