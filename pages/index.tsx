8dd00b4f05f32e2c132bd9899987d03f6714960e
John Roller <jtroller33@gmail.com>
2023-06-09 13:08:37 -0500

    Frontend is done, except for all the issues that need to be fixed on kanban

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
+import Link from 'next/link'
+
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
@@ -13,16 +15,16 @@ const inter = Inter({ subsets: ['latin'] })     // This does something with the 
 
 export default function Home() {
   return (
-    <div 
-    className='bg-[#000C18] text-[#f5f5f5c1] h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0
+    <div
+      className='bg-[#000C18] text-[#f5f5f5c1] h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0
     scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'
     >
       <Head>
-        <title>John Roller Portfolio</title> 
+        <title>John Roller Portfolio</title>
       </Head>
 
       <Header />
- 
+
       <section id="hero" className='snap-start scroll-smooth'>
         <Hero />
       </section>
@@ -48,6 +50,21 @@ export default function Home() {
       <section id='contact' className='snap-start'>
         <ContactMe />
       </section>
+
+      <Link href="#hero">
+        <footer className='sticky bottom-5 w-full cursor-pointer'>
+          <div className='flex items-center justify-center'>
+            <Image
+              className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
+              src='https://placehold.co/20x20/png'
+              alt='project'
+              width={20}
+              height={20}
+
+            />
+          </div>
+        </footer>
+      </Link>
 
     </div>
   )
